e641d7aa92bb9a9c6c78648dc5030375b763fafc
Dnoil <mr.danyavorontsov@yandex.ru>
2024-04-02 09:25:24 +0300

    Edit DB

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -12,10 +12,13 @@ CREATE TABLE user
     login            VARCHAR(255)            NOT NULL,
     password         VARCHAR(255)            NOT NULL,
     registered       TIMESTAMP DEFAULT now() NOT NULL,
-    enabled          BOOLEAN   DEFAULT TRUE  NOT NULL
+    enabled          BOOLEAN   DEFAULT TRUE  NOT NULL,
+    voted            BOOLEAN   DEFAULT FALSE NOT NULL,
+    voted_time       TIMESTAMP
 );
-CREATE UNIQUE INDEX user_unique_email_idx
-    ON user (email);
+CREATE UNIQUE INDEX user_unique_email_idx ON user (email);
+CREATE UNIQUE INDEX user_unique_login_idx ON user (login);
+CREATE UNIQUE INDEX user_unique_voted_idx ON user (voted);
 
 CREATE TABLE user_role
 (
@@ -32,6 +35,7 @@ CREATE TABLE restaurant
     address             VARCHAR(255)    NOT NULL,
     voted_popularity    VARCHAR(255)
 );
+CREATE UNIQUE INDEX restaurant_name_voted_idx ON restaurant (name);
 
 CREATE TABLE menu
 (
@@ -41,6 +45,7 @@ CREATE TABLE menu
     menu_restaurant     INTEGER         NOT NULL,
     FOREIGN KEY (menu_restaurant) REFERENCES restaurant (id)
 );
+CREATE UNIQUE INDEX menu_name_voted_idx ON menu (name);
 
 CREATE TABLE dish
 (
