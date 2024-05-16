039efa9e8a3c737fe82718020bd5bca5cbf83fcb
Dnoil <mr.danyavorontsov@yandex.ru>
2024-05-16 17:17:02 +0300

    Fix Authorization

--- src/main/java/com/github/dnoil/restaurant/voting/config/SecurityConfig.java
+++ src/main/java/com/github/dnoil/restaurant/voting/config/SecurityConfig.java
@@ -1,21 +1,18 @@
 package com.github.dnoil.restaurant.voting.config;
 
-import com.github.dnoil.restaurant.voting.error.NotFoundException;
 import com.github.dnoil.restaurant.voting.model.Role;
-import com.github.dnoil.restaurant.voting.model.User;
-import com.github.dnoil.restaurant.voting.repository.UserRepository;
-import com.github.dnoil.restaurant.voting.security.AuthorizedUser;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,23 +24,21 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-    private final UserRepository userRepository;
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return PASSWORD_ENCODER;
     }
 
-    @Bean
-    UserDetailsService userDetailsService() {
-        return email -> {
-            log.debug("Authenticating '{}'", email);
-            User user = userRepository.getByEmail(email);
-            if (user == null) {
-                throw new NotFoundException("Not found user with email " + email);
-            }
-            return new AuthorizedUser(user);
-        };
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user@gmail.com")
+                .password(PASSWORD_ENCODER.encode("password"))
+                .authorities(Role.USER)
+                .and()
+                .withUser("admin@gmail.com")
+                .password(PASSWORD_ENCODER.encode("admin"))
+                .authorities(Role.ADMIN, Role.USER);
     }
 
     @Bean
